f8d95541993b8a79e771d2147ddfb30f3aa9528c
Lu040703 <3327512620@qq.com>
2026-02-09 17:08:16 +0800

    Create config.sample.php file

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 配置文件示例
+ * 安装程序会自动生成 config.php
+ */
+return [
+    'installed' => false,
+    'db_host' => 'localhost',
+    'db_name' => 'your_database_name',
+    'db_user' => 'your_username',
+    'db_pass' => 'your_password',
+    'admin_email' => '3327512620@qq.com',
+];
